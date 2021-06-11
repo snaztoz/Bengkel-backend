103c36eb7724d6d12faf74eab24dad0fad89161e
snaztoz <hafidhmn@gmail.com>
2021-06-11 21:05:00 +0700

    Membuat API untuk mengambil bengkel terdekat.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
+const admin = require('firebase-admin');
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.getNearbyBengkel = functions.https
+    .onRequest(async (req, res) => {
+        const {lat, long} = req.query
+        if (!lat || !long) {
+            res.status(400).json({error: 'required URL query: lat & long'})
+        }
+
+        const collection = await admin.firestore()
+            .collection('bengkel')
+            .where('lokasi', '!=', null) // filter yang tidak memiliki field lokasi
+            .get()
+
+        const bengkelList = collection.docs
+            .map(doc => {
+                data = doc.data()
+                data.id = doc.id
+                data.jarak = Math.abs(Math.sqrt(
+                    Math.pow(data.lokasi.latitude - lat, 2)
+                    + Math.pow(data.lokasi.longitude - long, 2)
+                ))
+
+                return data
+            })
+            .sort((bengkel1, bengkel2) => bengkel1.jarak - bengkel2.jarak)
+
+        res.status(200).json({result: bengkelList})
+    })
