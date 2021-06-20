05cc8939ffb4741a6a155009533c92730075efd0
snaztoz <hafidhmn@gmail.com>
2021-06-21 00:13:26 +0700

    Membuat method seeder database.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const admin = require('firebase-admin')
+const faker = require('faker')
 const functions = require('firebase-functions')
 const geofire = require('geofire-common')
 
 admin.initializeApp()
+
+exports.seedDatabase = functions.https
+    .onRequest(async (req, res) => {
+        const {lat, long} = req.query
+        if (!lat || !long) {
+            res.status(400).json({error: 'required URL query: lat & long'})
+        }
+
+        const LATITUDE = Number(lat)
+        const LONGITUDE = Number(long)
+
+        const promises = []
+        let counter = 0
+
+        // generate 20 data dummy
+        for (let i = 0; i < 20; i++) {
+            const q = admin.firestore()
+                .collection('bengkel')
+                .add({
+                    nama: faker.company.companyName(),
+                    alamat: faker.address.streetAddress(),
+                    lokasi: {
+                        latitude: Number(faker.address.latitude(LATITUDE + 0.1, LATITUDE - 0.1, 6)),
+                        longitude: Number(faker.address.longitude(LONGITUDE + 0.1, LONGITUDE - 0.1, 6))
+                    }
+                })
+                .then(() => counter++)
+
+            promises.push(q)
+        }
+
+        await Promise.all(promises)
+        res.status(200).json({'msg': `success menambah ${counter} data`})
+    })
 
 exports.getNearbyBengkel = functions.https
     .onRequest(async (req, res) => {
